6161cb8cb6de249b1aea71f76d3a92e8ffad01e5
Vishrut Reddi <vishrutreddi@gmail.com>
2018-08-04 16:39:59 -0700

    Add Date and time (Moment) component with tests

---
+++ src/components/Moment/Moment.resource.js
@@ -0,0 +1,26 @@
+const LandingResources = {
+  Month: {
+    january: 'January',
+    febuary: 'Febuary',
+    march: 'March',
+    april: 'April',
+    may: 'June',
+    july: 'July',
+    august: 'August',
+    september: 'September',
+    october: 'October',
+    november: 'November',
+    december: 'December',
+  },
+  Day: {
+    monday: 'Monday',
+    tuesday: 'Tuesday',
+    wednesday: 'Wednesday',
+    thursday: 'Thursday',
+    friday: 'Friday',
+    saturday: 'Saturday',
+    sunday: 'Sunday',
+  },
+};
+
+export default LandingResources;
